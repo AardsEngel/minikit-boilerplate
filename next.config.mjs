b7245022bcf13452e81c02f325b44cdc9acad7ce
Gabriël <102593555+AardsEngel@users.noreply.github.com>
2025-06-20 00:17:17 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Silence warnings
+  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -13,6 +19,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
